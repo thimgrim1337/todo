db5ae67d8cebfcff5978f13c6b3b3dbef4a52eda
thimgrim1337 <11806430+thimgrim1337@users.noreply.github.com>
2023-03-06 19:54:36 +0100

    Implement display projects list and delete project function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 import './styles/main.scss';
+import App from './modules/App';
+
+const app = new App();
+
+function createProjectList() {
+  const projectsList = document.querySelector('.projects');
+  const projects = app.getAllProject();
+
+  projects.forEach((project) => {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    const i = document.createElement('i');
+    li.classList.add('project');
+    i.classList.add('fa-regular', 'fa-trash-can');
+
+    a.textContent = project.name;
+    a.href = '#';
+
+    li.append(a, i);
+    projectsList.appendChild(li);
+  });
+
+  const deleteProjectIcons = document.querySelectorAll('.fa-trash-can');
+  deleteProjectIcons.forEach((icon) =>
+    icon.addEventListener('click', () => {
+      if (app.deleteProject(icon.previousSibling.textContent)) {
+        icon.parentElement.remove();
+      }
+    })
+  );
+}
+
+createProjectList();
--- src/modules/App.js
+++ src/modules/App.js
@@ -1,8 +1,13 @@
 import Project from './Project';
 
 export default class App {
+  _defaultProjects = [
+    new Project('Main'),
+    new Project('Completed'),
+    new Project('Test'),
+  ];
   constructor() {
-    this.projects = [new Project('main')];
+    this.projects = [...this._defaultProjects];
   }
 
   checkName = (name) => {
@@ -13,11 +18,20 @@ export default class App {
   createProject = (name) =>
     this.projects.push(new Project(this.checkName(name)));
 
-  deleteProject = (name) =>
+  deleteProject = (name) => {
+    if (
+      name == this._defaultProjects[0].name ||
+      name == this._defaultProjects[1].name
+    )
+      return false;
+
     this.projects.splice(
-      this.projects.findIndex((project) => (project.name = name)),
+      this.projects.findIndex((project) => project.name == name),
       1
     );
+
+    return true;
+  };
 
   getAllProject = () => this.projects;
 }
