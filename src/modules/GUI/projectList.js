3242c8f413fdb7048362444256ee865c7643b4c1
thimgrim1337 <11806430+thimgrim1337@users.noreply.github.com>
2023-03-08 19:31:52 +0100

    Implement displayActiveProjectName and makeFieldEditable

--- src/modules/GUI/projectList.js
+++ src/modules/GUI/projectList.js
@@ -3,38 +3,41 @@ import { app } from '../../index';
 export default function initProjectList() {
   const projectsList = document.querySelector('.projects');
   const defaultProjects = app.getDefaultProjects();
-  const label = document.querySelector('[for="newProject"]');
-  const input = document.querySelector('input#newProject');
+  const newProjectLabel = document.querySelector('[for="newProject"]');
+  const newProjectInput = document.querySelector('input#newProject');
 
   defaultProjects.forEach((project) => {
     projectsList.appendChild(createProjectListItem(project.name));
   });
 
+  const mainProject = document.querySelector('.project');
+  mainProject.classList.add('active');
+  displayActiveProjectName();
+
   document.querySelector('.btn-project').addEventListener('click', () => {
-    label.classList.toggle('visible');
+    newProjectLabel.classList.toggle('visible');
   });
 
   document.querySelector('.fa-check').addEventListener('click', () => {
-    createProject(input.value) && clearInput(input);
+    createProject(newProjectInput.value) && clearInput(newProjectInput);
   });
 
   document.querySelector('.fa-xmark').addEventListener('click', clearInput);
   function clearInput() {
-    input.value = '';
-    label.classList.remove('visible');
+    newProjectInput.value = '';
+    newProjectLabel.classList.remove('visible');
   }
 }
 
 function createProjectListItem(name) {
   const li = document.createElement('li');
-  const a = document.createElement('a');
 
+  li.textContent = name;
   li.classList.add('project');
 
-  a.textContent = name;
-  a.href = '#';
+  li.addEventListener('click', setActive);
+  li.addEventListener('dblclick', makeFieldEditable);
 
-  li.appendChild(a);
   return li;
 }
 
@@ -56,4 +59,45 @@ function createProject(name) {
   projectsList.appendChild(project);
   app.createProject(name);
   return true;
+}
+
+function setActive(e) {
+  removeActiveClass();
+  e.target.classList.add('active');
+  displayActiveProjectName();
+}
+
+function removeActiveClass() {
+  document
+    .querySelectorAll('.project')
+    .forEach((project) => project.classList.remove('active'));
+}
+
+function displayActiveProjectName() {
+  document.querySelector('#activeProjectName').textContent =
+    document.querySelector('.active').textContent;
+}
+
+function makeFieldEditable(e) {
+  const currentElement = e.target;
+  const currentName = e.target.textContent;
+  const input = document.createElement('input');
+  input.type = 'text';
+
+  e.target.parentElement.replaceChild(input, currentElement);
+  input.value = currentName;
+
+  input.addEventListener('keydown', changeName);
+  input.addEventListener('focusout', changeName);
+
+  function changeName(e) {
+    if ((e.type == 'keydown' && e.key == 'Enter') || e.type == 'focusout') {
+      const newName = e.target.value;
+      currentElement.textContent = newName;
+      e.target.parentElement.replaceChild(currentElement, input);
+
+      input.removeEventListener('keydown', inputChange);
+      input.removeEventListener('focusout', inputChange);
+    }
+  }
 }
